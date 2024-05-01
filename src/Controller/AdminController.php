9f270e8324f9aae7e26d249b6ad1c7072d473714
Leith Engazzou <147639906+LeithEng@users.noreply.github.com>
2024-05-01 19:37:20 +0100

    Update AdminController.php

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -1,24 +1,46 @@
 <?php
 
 namespace App\Controller;
-
+use App\Form\AdminMailType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
-    #[Route('/admin/dashboard', name: 'admin_dashboard')]
-    public function dashboard(): Response
-    {
 
-        $pageTitle = 'Admin Dashboard';
+
+    #[Route('/admin/dashboard', name: 'send_email')]
+    public function sendEmail(Request $request): Response
+    {
+        $form = $this->createForm(AdminMailType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $formData = $form->getData();
+            $recipient = $formData['recipient'];
+            $subject = $formData['subject'];
+            $message = $formData['message'];
+            $email = (new \Symfony\Component\Mime\Email())
+                ->from('your@example.com')
+                ->to($recipient)
+                ->subject($subject)
+                ->text($message);
+
+            $mailer = $this->get('mailer');
+            $mailer->send($email);
+
+            // Redirect or display success message
+            $this->addFlash('success', 'Email sent successfully.');
+
+        }
 
         return $this->render('admin/dashboard.html.twig', [
-            'page_title' => $pageTitle,
-            // Add more variables as needed
+            'eform' => $form->createView(),
         ]);
+
+
     }
-
-
 }
