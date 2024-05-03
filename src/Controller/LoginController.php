e989825a43057485a2e2a2e32b1c57486f88086d
Leith Engazzou <147639906+LeithEng@users.noreply.github.com>
2024-05-03 17:00:00 +0100

    Update LoginController.php

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -1,28 +1,74 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Utilisateur;
 use App\Form\LoginType;
 use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Psr\Log\LoggerInterface;
 
 class LoginController extends AbstractController
 {
 #[Route('/login', name: 'login_page')]
-public function login(Request $request)
+public function login(Request $request,EntityManagerInterface $entityManager,LoggerInterface $logger)
 {
+
+
+
 $loginForm = $this->createForm(LoginType::class);
 $registrationForm = $this->createForm(RegistrationType::class);
-
 $loginForm->handleRequest($request);
 $registrationForm->handleRequest($request);
-
-if ($loginForm->isSubmitted() && $loginForm->isValid()) {
-
+if ($loginForm->isSubmitted() && $loginForm->isValid() ) {
+    $user=new Utilisateur();
+    $formData = $loginForm->getData();
+    $email = $formData->getEmail();
+    var_dump($email);
+    $password = $formData->getPass();
+    var_dump($password);
+    $user = $entityManager->getRepository(Utilisateur::class)->findOneBy(['email' => $email]);
+    if (!$user) {
+        $this->addFlash('danger', 'Invalid credentials.');
+    }
+    else{
+        $storedPassword = $user->getPass();
+        if ($password=== $storedPassword) {
+            if(!$user->get_is_Admin())
+                return $this->redirectToRoute('home');
+            else
+                return $this->redirectToRoute('dashboard');
+        } else {
+            $this->addFlash('danger', 'Invalid credentials.');
+        }
+    }
 }
 
-if ($registrationForm->isSubmitted() && $registrationForm->isValid()) {
+if ($registrationForm->isSubmitted() && $registrationForm->isValid() && !$loginForm->isSubmitted()) {
+$user=new Utilisateur();
+    $formData = $registrationForm->getData();
+    $email = $formData->getEmail();
+    $user = $entityManager->getRepository(Utilisateur::class)->findOneBy(['email' => $email]);
+    if(!$user)
+    {   $newuser=new Utilisateur();
+        $newuser->setNom($formData->getNom());
+        $newuser->setPrenom($formData->getPrenom());
+        $newuser->setPass($formData->getPass());
+        $newuser->setEmail($email);
+        $newuser->setNumTel($formData->getNumTel());
+        $newuser->set_is_Banned(false);
+        $newuser->set_is_Admin(false);
+        $entityManager->persist($newuser);
+        $entityManager->flush();
+        $this->addFlash('success', 'Registration successful!');
+        return $this->redirectToRoute('home');
+    }
+    else{
+        $this->addFlash('danger', 'This email is already in use');
+    }
+
 
 }
 
