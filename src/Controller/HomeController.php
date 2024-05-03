1023a7d0f328f389e1ea6c762e618e7626b635fa
MedSahbiBenKalia <mohamedsahbi.benkalia@insat.ucar.tn>
2024-05-03 23:18:34 +0100

    adding home.html.twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,7 +11,7 @@ class HomeController extends AbstractController
     #[Route('/home', name: 'home')]
     public function index(): Response
     {
-        return $this->render('client/home.html.twig', [
+        return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
