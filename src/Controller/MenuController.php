fdb5a7a5d21337eb0a60307ae99a1e7335368c35
MedSahbiBenKalia <mohamedsahbi.benkalia@insat.ucar.tn>
2024-05-05 00:32:30 +0100

    update fragments file

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -13,29 +13,68 @@ use Symfony\Component\Routing\Attribute\Route;
 class MenuController extends AbstractController
 {
 
-    #[Route('/menu', name: 'menu')]
+#[Route('/menu', name: 'menu')]
     public function index( Request $request ): Response
     {
-        $session=$request->getSession();
-        $cart=$session->get('cart');
         $form = $this->createForm(ProductType::class);
         $form->handleRequest($request);
-
-        $form1 = $this->createForm(ProductType::class);
-        $form1->handleRequest($request);
-
-
-
         if ($form->isSubmitted() && $form->isValid()) {
-
             return $this->add($request, $form);
         }
 
+        $form1 = $this->createForm(ProductType::class);
+        $form1->handleRequest($request);
         if ($form1->isSubmitted() && $form1->isValid()) {
-
             return $this->add($request, $form1);
         }
 
+        $form2 = $this->createForm(ProductType::class);
+        $form2->handleRequest($request);
+        if ($form2->isSubmitted() && $form2->isValid()) {
+            return $this->add($request, $form2);
+        }
+
+        $form3 = $this->createForm(ProductType::class);
+        $form3->handleRequest($request);
+        if ($form3->isSubmitted() && $form3->isValid()) {
+            return $this->add($request, $form3);
+        }
+
+        $form4 = $this->createForm(ProductType::class);
+        $form4->handleRequest($request);
+        if ($form4->isSubmitted() && $form4->isValid()) {
+            return $this->add($request, $form4);
+        }
+
+        $form5 = $this->createForm(ProductType::class);
+        $form5->handleRequest($request);
+        if ($form5->isSubmitted() && $form5->isValid()) {
+            return $this->add($request, $form5);
+        }
+
+        $form6 = $this->createForm(ProductType::class);
+        $form6->handleRequest($request);
+        if ($form6->isSubmitted() && $form6->isValid()) {
+            return $this->add($request, $form6);
+        }
+
+        $form7 = $this->createForm(ProductType::class);
+        $form7->handleRequest($request);
+        if ($form7->isSubmitted() && $form7->isValid()) {
+            return $this->add($request, $form7);
+        }
+
+        $form8 = $this->createForm(ProductType::class);
+        $form8->handleRequest($request);
+        if ($form8->isSubmitted() && $form8->isValid()) {
+            return $this->add($request, $form8);
+        }
+
+        $form9 = $this->createForm(ProductType::class);
+        $form9->handleRequest($request);
+        if ($form9->isSubmitted() && $form9->isValid()) {
+            return $this->add($request, $form9);
+        }
 
 
 
@@ -43,7 +82,16 @@ class MenuController extends AbstractController
         return $this->render('menu/index.html.twig', [
             'controller_name' => 'MenuController',
             'form' => $form->createView(),
-            'form1' => $form1->createView()
+            'form1' => $form1->createView(),
+            'form2' => $form2->createView(),
+            'form3' => $form3->createView(),
+            'form4' => $form3->createView(),
+            'form5' => $form3->createView(),
+            'form6' => $form3->createView(),
+            'form7' => $form3->createView(),
+            'form8' => $form3->createView(),
+            'form9' => $form3->createView()
+
 
         ]);
     }
