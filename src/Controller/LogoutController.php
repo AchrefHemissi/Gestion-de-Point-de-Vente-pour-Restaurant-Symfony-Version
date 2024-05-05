ea5462e39d82a61e3d67f57e6fa98875d90ec887
Leith Engazzou <147639906+LeithEng@users.noreply.github.com>
2024-05-05 12:47:11 +0100

    Update LogoutController.php

--- src/Controller/LogoutController.php
+++ src/Controller/LogoutController.php
@@ -3,16 +3,21 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class LogoutController extends AbstractController
 {
     #[Route('/logout', name: 'logout')]
-    public function index(): Response
-    {
-        return $this->render('logout/index.html.twig', [
-            'controller_name' => 'LogoutController',
-        ]);
+    public function index(Request $request): Response
+    {   
+        $session = $request->getSession();
+        if ($session->has('id')) {
+            $session->remove('id');
+        }
+
+
+        return $this->redirectToRoute('login_page');
     }
 }
