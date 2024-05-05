7a5dc745b0855d81ea8a98373d60934a9a9c415f
Leith Engazzou <147639906+LeithEng@users.noreply.github.com>
2024-05-05 13:37:12 +0100

    Update AboutController.php

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -3,14 +3,19 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class AboutController extends AbstractController
 {
     #[Route('/about', name: 'about')]
-    public function index(): Response
-    {
+    public function index(Request $request): Response
+    {   $session = $request->getSession();
+        $id = $session->get('id');
+        if(!$id)
+            return $this->redirectToRoute('login_page');
+
         return $this->render('about/index.html.twig', [
             'controller_name' => 'AboutController',
         ]);
