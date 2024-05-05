0c354bc0de03b0b701a575ff7d2b92246c866d24
Leith Engazzou <147639906+LeithEng@users.noreply.github.com>
2024-05-05 13:36:53 +0100

    Update CartController.php

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,14 +3,19 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class CartController extends AbstractController
 {
     #[Route('/cart', name: 'cart')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $session = $request->getSession();
+        $id = $session->get('id');
+        if(!$id)
+            return $this->redirectToRoute('login_page');
         return $this->render('cart/index.html.twig', [
             'controller_name' => 'CartController',
         ]);
