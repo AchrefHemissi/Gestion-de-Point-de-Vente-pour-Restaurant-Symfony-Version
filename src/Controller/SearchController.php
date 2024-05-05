3c941c2faa32242fd45333a62e331afdfb227499
Leith Engazzou <147639906+LeithEng@users.noreply.github.com>
2024-05-05 13:33:39 +0100

    Update SearchController.php

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -3,14 +3,18 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class SearchController extends AbstractController
 {
     #[Route('/search', name: 'search')]
-    public function index(): Response
-    {
+    public function index(Request $request): Response
+    {   $session=$request->getSession();
+        $person=$session->get('id');
+        if(!$person)
+            return $this->redirectToRoute('login_page');
         return $this->render('search/index.html.twig', [
             'controller_name' => 'SearchController',
         ]);
