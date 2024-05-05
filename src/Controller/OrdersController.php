63079e65246e714a1f544412d19edc6dbf953c79
Leith Engazzou <147639906+LeithEng@users.noreply.github.com>
2024-05-05 13:35:01 +0100

    Update OrdersController.php

--- src/Controller/OrdersController.php
+++ src/Controller/OrdersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +10,12 @@ use Symfony\Component\Routing\Attribute\Route;
 class OrdersController extends AbstractController
 {
     #[Route('/orders', name: 'orders')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $session=$request->getSession();
+        $person=$session->get('id');
+        if(!$person)
+            return $this->redirectToRoute('login_page');
         return $this->render('orders/index.html.twig', [
             'controller_name' => 'OrdersController',
         ]);
