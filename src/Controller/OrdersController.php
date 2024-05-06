c27f798e3ee9756a0fa7d8e3b9f628e01d926cff
Leith Engazzou <147639906+LeithEng@users.noreply.github.com>
2024-05-06 23:29:01 +0100

    Update OrdersController.php

--- src/Controller/OrdersController.php
+++ src/Controller/OrdersController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Commande;
+use App\Entity\Utilisateur;
+use Doctrine\Common\Collections\Order;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,12 +14,22 @@ use Symfony\Component\Routing\Attribute\Route;
 class OrdersController extends AbstractController
 {
     #[Route('/orders', name: 'orders')]
-    public function index(Request $request): Response
-    {
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
+    {   $repository = $entityManager->getRepository(Utilisateur::class);
         $session=$request->getSession();
-        $person=$session->get('id');
+        $person=$repository->findOneBy(['id'=>$session->get('id')]);
         if(!$person)
             return $this->redirectToRoute('login_page');
+        $order=new Commande();
+        $paymentdata=$session->get('payment_data');
+        $orderdate=new \DateTime($paymentdata['date']);
+        $orderaddress=$paymentdata['address'];
+        $order->setLieu($orderaddress);
+        $order->setDateCommande($orderdate);
+        $order->setIdClient($person);
+        $order->setEtatCommande(0);
+        $entityManager->persist($order);
+        $entityManager->flush();
         return $this->render('orders/index.html.twig', [
             'controller_name' => 'OrdersController',
         ]);
